28d472c981516051e0d0a998c6b0e1fc4c68dfa7
Ralf Baussenwein <ralf@bwein.net>
2024-03-07 00:40:15 +0100

    Fix icons for contao 5

--- src/FrontendHooks.php
+++ src/FrontendHooks.php
@@ -160,7 +160,7 @@ class FrontendHooks
 					$data['links']['be-module'] = array(
 						'url' => static::getBackendURL('news', 'tl_content', $matches2[2], false),
 						'label' => sprintf(is_array($GLOBALS['TL_LANG']['tl_news']['edit']) ? $GLOBALS['TL_LANG']['tl_news']['edit'][1] : $GLOBALS['TL_LANG']['tl_news']['edit'], $matches2[2]),
-						'icon' => Image::getPath('news.svg'),
+						'icon' => Image::getPath('bundles/contaonews/news.svg'),
 					);
 				}
 
@@ -178,7 +178,7 @@ class FrontendHooks
 					$data['links']['be-module'] = array(
 						'url' => static::getBackendURL('calendar', 'tl_content', $matches2[2], false),
 						'label' => sprintf(is_array($GLOBALS['TL_LANG']['tl_calendar_events']['edit']) ? $GLOBALS['TL_LANG']['tl_calendar_events']['edit'][1] : $GLOBALS['TL_LANG']['tl_calendar_events']['edit'], $matches2[2]),
-						'icon' => Image::getPath('settings.svg'),
+						'icon' => Image::getPath('bundles/contaocalendar/calendar.svg'),
 					);
 				}
 
